9d95ba6f1bc5b89822b13d71c9bd81973197dbef
Vinícius <vinigperuzzi@hotmail.com>
2024-09-04 14:03:57 -0300

    Finishin starter tests

--- test2.c
+++ test2.c
@@ -2,11 +2,12 @@
 #include <unistd.h>//Para utilizar o sleep
 #include <pthread.h>
 #include <stdlib.h>
+#include <sys/syscall.h>
 #define NUM_THREADS 5
 
 void *printHello(void *threadid){
     int tid = *(int*)threadid;
-    printf("Olá! Sou a thread #%d!\n", tid);
+    printf("Olá! Sou a thread #%d(%d)!\nNo sistema sou <%ld>\nE meu PID no sistema sou %lu\n", tid, getpid(), pthread_self(), syscall(__NR_gettid));
     int teste = ((25 + 1) * (45 * 3)) / 3;
     printf("%d", teste);
     pthread_exit(NULL);
